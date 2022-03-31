1bfab4cdf4bfef43cf2145a30f6da68cd8e6de8f
amandatsantos <tavaresamandasantos@hotmail.com>
2022-03-30 22:09:20 -0300

    exc cadastro

---
+++ exercicios_funcoes/exc_cadastro/agenda.py
@@ -0,0 +1,61 @@
+database = {}
+
+def mostrar_registros ():
+    if len(database) == 0:
+        print(' \nbanco de dados esta vazio, Não existe nenhum registro \n')
+    
+    else: 
+        for registro in database:
+            print('-'*30)
+            buscar_registro(registro)
+            print('-'*30)
+
+def buscar_registro(registro):
+    if registro not in database:
+        print(f' \n registro de  : {registro} não foi encontrado na base de dados  \n')
+    else:
+        print('-'*30)
+        print('Nome:', registro)
+        print('telefone:', database[registro]['telefone'])
+        print('endereco:', database[registro]['endereco'])
+        print('email:', database[registro]['email'])
+        print('data_nascimento:', database[registro]['data_nascimento'])
+        print('-'*30)
+
+
+def adicionar_editar_registro (registro, telefone, endereco, email, data_nascimento ):
+    
+    database[registro] = {
+        'telefone': telefone,
+        'endereco': endereco,
+        'email': email,
+        'data_nascimento': data_nascimento,
+    }
+
+    print(f"\n ------- registro de {registro} incluido com sucesso !! ------- \n")
+
+def excluir_registro(registro):
+    if registro in database :
+        database.pop(registro)
+        print(f'\n ------ o registro de {registro} foi excluido com sucesso ----- \n')
+    else: 
+        print(f'\nregistro não encontrado {registro}\n')
+    
+    
+
+def salvar_registro_arquivo ():
+    try : 
+        with open ('exercicios_funcoes/cadastro/repositorio_registros/registros.txt', 'a') as arq:
+            arq.writelines ("registro\t | telefone\t | endereco\t | email\t | data_nascimento\n")
+            for registro in database:
+                telefone = database[registro]['telefone']
+                endereco = database[registro]['endereco']
+                email = database[registro]['email']
+                data_nascimento = database[registro]['data_nascimento']
+                
+                arq.writelines (f'{registro}\t\t | {telefone}\t\t | {endereco}\t\t | {email}\t\t | {data_nascimento}\n')
+            print('---- registros salvos em arquivo com sucesso !!! ')
+
+            
+    except KeyError as error :
+            print(f" ----- erro !!! ocorreu algum erro ao salrvar aquivo dos registros  {error}!!")
---
+++ exercicios_funcoes/exc_cadastro/menu.py
@@ -0,0 +1,99 @@
+from agenda import *
+
+usuario = input("Entre com o seu nome: ")
+print(f"Seja Bem-vindo {usuario} !\n")
+
+while True:
+    print("Selecione uma opção:")
+    print("1 - Cadastrar")
+    print("2 - mostrar registros")
+    print("3 - buscar registro")
+    print("4 - editar registro")
+    print("5 - excluir_registro")
+    print("6 - salvar em arquivo os registos")
+    print(" 7 - sair")
+
+    opcao = int(input(""))
+    if opcao in [1,2,3,4,5,6, 7]:
+        if opcao == 1: #Cadastrar
+            registro = input("insira o nome do registro que deseja adicionar:\n ")
+            while registro == "":
+                    registro = input("Coloque o nome: ")
+            try: 
+                database [ registro]
+                print(f' --- registro com esse nome {registro} já existe')
+            except :
+                telefone = ""
+                endereco = ""
+                email = ""
+                data_nascimento = ""
+                
+                while telefone == "":
+                    telefone= input("Coloque o telefone: ")
+                while endereco == "":
+                    endereco = input("Coloque o endereço: ")
+                while email == "":
+                    email = input("Coloque o email: ")
+                while data_nascimento == "":
+                    data_nascimento = input("Coloque a data nascimento: ")
+
+            adicionar_editar_registro(registro, telefone, endereco, email,data_nascimento)
+            
+            
+        elif opcao == 2: #mostrar registros
+            mostrar_registros()
+            
+        elif opcao == 3:#buscar registros
+            registro = input('insira o nome do registro para a busca:\n ')
+            buscar_registro(registro)
+
+        elif opcao == 4:# editar registro
+            registro = input("insira o nome do registro que deseja alterar: ")
+            while registro == "":
+                    registro = input("Coloque o nome: ")
+            try: 
+                database [ registro]
+                print(f' --- editando o registro identificado como : {registro} ')
+            
+                while True:
+                    startar = input("\naqui o registro ira ser sobre-escrito, tenha certeza antes de atualiza-lo ... pressione enter para continuar\n")
+                    opcao  = int(input("\ndeseja continuar e atualizar registro? -aperte - 1\ndeseja sair da opcao editar contato ?- aperte - 2 \n"))
+                    if opcao == 1: 
+                        
+                        telefone = ""
+                        endereco = ""
+                        email = ""
+                        data_nascimento = ""
+                        
+                        while telefone == "":
+                            telefone= input("Coloque o telefone: ")
+                        while endereco == "":
+                            endereco = input("Coloque o endereço: ")
+                        while email == "":
+                            email = input("Coloque a email: ")
+                        while data_nascimento == "":
+                            data_nascimento = input("Coloque a data de nascimento: ")
+                        adicionar_editar_registro(registro, telefone, endereco, email,data_nascimento)
+                    elif opcao == 2 :
+                        print(" \natualização/edição não confirmada, voltando para o menu inicial\n")
+                        break
+                    else:
+                        ('opção invalida')
+            except KeyError:
+                    print("\nregistro não existe na base de dados\n")
+                    
+                
+
+        elif opcao == 5: # excluir registro
+            
+            registro = input('insira o nome do registro que deseja excluir:\n ')
+            excluir_registro(registro)
+        
+        elif opcao == 6: # salvar arquiv
+            salvar_registro_arquivo()
+
+        elif opcao == 7:#Sair        
+            print("Saindo do sistema...")
+            break
+    else:
+        print("Opção Inválida!")
